792f32bc88d0f8c2ad126ebfa4c41221e851e3a0
Hanut Arora <36478891+ShellXploit@users.noreply.github.com>
2024-04-13 19:03:53 +0530

    fix(utils): :lock: better way of getting random secret

--- src/utils/getSignedJwt.ts
+++ src/utils/getSignedJwt.ts
@@ -1,14 +1,13 @@
 import { sign } from "jsonwebtoken";
 
 import { GetSignedJwtProps } from "../types/utils/getSignedJwt";
+import { getUUID } from "./getUUID";
 
 export const getSignedJwt = ({
 	payload,
 	secret,
 	options
 }: GetSignedJwtProps): string => {
-	const secretValue = secret
-		? secret
-		: Math.random().toString(36).substring(2, 15);
+	const secretValue = secret ? secret : getUUID();
 	return sign(payload, secretValue, options);
 };
