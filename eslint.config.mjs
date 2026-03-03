d6f428da5f887caa530f98a57ff03953cc032b55
ShellXploit <shellxploit@gmail.com>
2026-03-03 20:11:22 +0530

    fix: remove @eslint/js dep from flat config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,13 +1,10 @@
-import js from "@eslint/js";
 import tsPlugin from "@typescript-eslint/eslint-plugin";
 import tsParser from "@typescript-eslint/parser";
-import globals from "globals";
 
 export default [
 	{
 		ignores: ["coverage/", "dist/", "node_modules/", "tests/", "tsconfig.json", "tsup.config.ts"]
 	},
-	js.configs.recommended,
 	{
 		files: ["**/*.ts"],
 		plugins: {
@@ -18,10 +15,6 @@ export default [
 			parserOptions: {
 				ecmaVersion: "latest",
 				sourceType: "module"
-			},
-			globals: {
-				...globals.node,
-				...globals.es2021
 			}
 		},
 		rules: {
